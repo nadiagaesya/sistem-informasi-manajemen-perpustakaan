8cea3fc6736ac077407d21c987e51e33735e4bf0
pangidoannsh <pangidoanhrp13@gmail.com>
2024-05-04 22:44:09 +0700

    fix:export excel data peminjaman

--- app/Exports/PeminjamanExport.php
+++ app/Exports/PeminjamanExport.php
@@ -3,29 +3,49 @@
 namespace App\Exports;
 
 use App\Models\Peminjaman;
+use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
-class PeminjamanExport implements FromView, WithMapping
+class PeminjamanExport implements FromCollection, WithHeadings, WithMapping, ShouldAutoSize
 {
-      public function view(): View
+      public function collection()
       {
-            $peminjamans = Peminjaman::all();
-
-            return view('exports.peminjaman', [
-                  'peminjamans' => $peminjamans
-            ]);
+            $model =  Peminjaman::join("users", "peminjam_id", "users.id")
+                  ->select(
+                        "kode_pinjam",
+                        "users.name",
+                        "tanggal_pinjam",
+                        "tanggal_kembali",
+                        "status",
+                        "denda",
+                  )
+                  ->get();
+            return $model;
       }
 
+      public function headings(): array
+      {
+            return [
+                  'Kode Pinjam',
+                  'Nama Peminjam',
+                  'Tanggal Pinjam',
+                  'Tanggal Kembali',
+                  'Status',
+                  'Denda',
+            ];
+      }
       public function map($peminjaman): array
       {
             return [
-                  $peminjaman->id,
-                  $peminjaman->nama,
                   $peminjaman->kode_pinjam,
-                  $peminjaman->tanggal_pinjam,
-                  $peminjaman->tanggal_kembali,
+                  $peminjaman->name,
+                  Carbon::parse($peminjaman->tanggal_pinjam)->format('d/m/Y'),
+                  Carbon::parse($peminjaman->tanggal_kembali)->format('d/m/Y'),
                   $peminjaman->status,
                   $peminjaman->denda,
             ];
